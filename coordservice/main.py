e593792197a8d0ba632b8fc67fd4dba518adf51c
Sean McLeaish <smcleaish@gmail.com>
2024-03-24 10:31:02 -0500

    fixed type issues, unit test was garbage, going to use the test library from fastapi

--- coordservice/main.py
+++ coordservice/main.py
@@ -1,21 +1,29 @@
-import asyncio
-import json
+"""
+This module contains the FastAPI application.
+"""
+
 import shutil
 import uuid
 from pathlib import Path
 
 from coordextract import process_coords
+from coordextract.point import PointModel
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+
 @app.post("/process-gpx/")
-async def process_gpx(file: UploadFile = File(...)):
+async def process_gpx(
+    file: UploadFile = File(...),
+) -> str | list[PointModel] | None:
+    """
+    Process a GPX file and return the coordinates.
+    """
     temp_file = Path(f"temp_{uuid.uuid4()}_{file.filename}")
     with temp_file.open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     result = await process_coords(temp_file, indentation=None)
     temp_file.unlink()
 
-    return {"data": result}
+    return result
